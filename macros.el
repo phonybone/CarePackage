5d869fb19b7209b5abc6c535ffbe786ca208b081
Victor Cassen <victor.cassen@zonarsystems.com>
2020-05-26 09:49:15 -0700

    add macros.el

---
+++ macros.el
@@ -0,0 +1,2 @@
+(fset 'tide-fix-indentation
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([home end 134217756 home 24 37 tab return 32 32 32 32 32 32 32 32 return home 24 37 32 32 32 32 return 32 32 return] 0 "%d")) arg)))
